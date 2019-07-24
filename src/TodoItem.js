bfb1907fd900b5bf5892e946620e3965296e5567
Max <max@chuckwalladesign.com>
2019-07-24 16:44:11 -0700

    Change checkbox paragraphs to labels for better clickability.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -9,12 +9,14 @@ function TodoItem(props) {
 
     return (
         <div className="todo-item">
-            <input
-                type="checkbox"
-                checked={props.item.completed}
-                onChange={() => props.handleChange(props.item.id)}
-            />
-            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
+            <label style={props.item.completed ? completedStyle: null}>
+              <input
+                  type="checkbox"
+                  checked={props.item.completed}
+                  onChange={() => props.handleChange(props.item.id)}
+              />
+              {props.item.text}
+            </label>
         </div>
     )
 }
